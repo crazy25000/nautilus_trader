59282e7c617c5b59e8f1f27361562cc8aa09112a
Nazim <crazy25000@gmail.com>
2021-07-20 23:10:17 -0400

    REF: Refactor adaptor

--- examples/live/oanda_ema_cross.py
+++ examples/live/oanda_ema_cross.py
@@ -1,32 +1,14 @@
-#!/usr/bin/env python3
-# -------------------------------------------------------------------------------------------------
-#  Copyright (C) 2015-2021 Nautech Systems Pty Ltd. All rights reserved.
-#  https://nautechsystems.io
-#
-#  Licensed under the GNU Lesser General Public License Version 3.0 (the "License");
-#  You may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
-# -------------------------------------------------------------------------------------------------
-
 from decimal import Decimal
 import os
 import sys
 
 
-sys.path.insert(
-    0, str(os.path.abspath(__file__ + "/../../../"))
-)  # Allows relative imports from examples
+sys.path.insert(0, str(os.path.abspath(__file__ + "/../../../")))
 
 from examples.strategies.ema_cross_simple import EMACross
 from nautilus_trader.adapters.oanda.factories import OandaDataClientFactory
 from nautilus_trader.live.node import TradingNode
-from nautilus_trader.model.bar import BarSpecification
+from nautilus_trader.model.data.bar import BarSpecification
 from nautilus_trader.model.enums import BarAggregation
 from nautilus_trader.model.enums import PriceType
 from nautilus_trader.model.identifiers import InstrumentId
@@ -34,57 +16,37 @@ from nautilus_trader.model.identifiers import Symbol
 from nautilus_trader.model.identifiers import Venue
 
 
-# TODO: OandaExecutionClientFactory
-
-
-# The configuration dictionary can come from anywhere such as a JSON or YAML
-# file. Here it is hardcoded into the example for clarity.
-config = {
-    "trader": {
-        "name": "TESTER",  # Not sent beyond system boundary
-        "id_tag": "001",  # Used to ensure orders are unique for this trader
+config = dict(
+    trader={"name": "TESTER", "id_tag": "001"},
+    system={
+        "loop_debug": True,
+        "timeout_connection": 100000000.0,
+        "timeout_reconciliation": 100000000.0,
+        "timeout_portfolio": 100000000.0,
+        "timeout_disconnection": 5.0,
+        "check_residuals_delay": 5.0,
     },
-    "system": {
-        "loop_debug": False,  # If event loop debug mode
-        "timeout_connection": 10.0,  # Timeout for all clients to connect and initialize
-        "timeout_reconciliation": 10.0,  # Timeout for execution state to reconcile
-        "timeout_portfolio": 10.0,  # Timeout for portfolio to initialize margins and unrealized PnLs
-        "timeout_disconnection": 5.0,  # Timeout for all engine clients to disconnect
-        "check_residuals_delay": 5.0,  # Delay to await residual events after stopping engines
-    },
-    "logging": {
-        "level_stdout": "INF",
-    },
-    "database": {
+    logging={"level_stdout": "INF"},
+    database={
         "type": "redis",
         "host": "localhost",
         "port": 6379,
     },
-    "data_engine": {},
-    "risk_engine": {},
-    "exec_engine": {},
-    "strategy": {
-        "load_state": True,  # Strategy state is loaded from the database on start
-        "save_state": True,  # Strategy state is saved to the database on shutdown
+    data_engine={},
+    risk_engine={},
+    exec_engine={},
+    strategy={
+        "load_state": False,
+        "save_state": False,
     },
-    "data_clients": {
+    data_clients={
         "OANDA": {
-            "api_token": "OANDA_API_TOKEN",  # value is the environment variable key
-            "account_id": "OANDA_ACCOUNT_ID",  # value is the environment variable key
+            "api_token": "OANDA_API_TOKEN",
+            "account_id": "OANDA_ACCOUNT_ID",
         },
     },
-    "exec_clients": {
-        # "OANDA": {
-        #     "api_token": "OANDA_API_TOKEN",  # value is the environment variable key
-        #     "account_id": "OANDA_ACCOUNT_ID",  # value is the environment variable key
-        # },
-    },
-}
-
-
-# Instantiate your strategies to pass into the trading node. You could add
-# custom options into the configuration file or even use another configuration
-# file.
+    exec_clients={},
+)
 
 instrument1 = InstrumentId(
     symbol=Symbol("AUD/USD"),
@@ -99,54 +61,12 @@ strategy1 = EMACross(
     trade_size=Decimal(10000),
     order_id_tag="001",
 )
-
-# ------------------------------------------------------------------------------
-
-instrument2 = InstrumentId(
-    symbol=Symbol("EUR/USD"),
-    venue=Venue("OANDA"),
-)
-
-strategy2 = EMACross(
-    instrument_id=instrument2,
-    bar_spec=BarSpecification(1, BarAggregation.MINUTE, PriceType.MID),
-    fast_ema_period=10,
-    slow_ema_period=20,
-    trade_size=Decimal(10000),
-    order_id_tag="002",
-)
-
-# ------------------------------------------------------------------------------
-
-instrument3 = InstrumentId(
-    symbol=Symbol("GBP/USD"),
-    venue=Venue("OANDA"),
-)
-
-strategy3 = EMACross(
-    instrument_id=instrument3,
-    bar_spec=BarSpecification(1, BarAggregation.MINUTE, PriceType.MID),
-    fast_ema_period=10,
-    slow_ema_period=20,
-    trade_size=Decimal(10000),
-    order_id_tag="003",
-)
-
-strategies = [
-    strategy1,
-    strategy2,
-    strategy3,
-]
-
-# Instantiate the node passing a list of strategies and configuration
-node = TradingNode(strategies=strategies, config=config)
-
-# Register your client factories with the node (can take user defined factories)
+print(config)
+node = TradingNode(strategies=[strategy1], config={**config})
 node.add_data_client_factory("OANDA", OandaDataClientFactory)
 node.build()
 
 
-# Stop and dispose of the node with SIGINT/CTRL+C
 if __name__ == "__main__":
     try:
         node.start()
